94ac79f495f26795782e3e568c5d064323075c7d
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-20 17:33:01 -0300

    Atividade 01/02/03 (segui os videos)

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = comprimento * largura * altura;
+
+            Console.WriteLine($"O volume da caixa é de {resultado} cm³.");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quilometragem inicial: ");
+            double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quilometragem final: ");
+            double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o consumo de combustível após a viagem (litros): ");
+            double consumoCombustivel = Convert.ToDouble(Console.ReadLine());
+
+            double distanciaPercorrida = kmFinal - kmInicial;
+
+            double consumoPorKm = distanciaPercorrida / consumoCombustivel;
+
+            Console.WriteLine($"O consumo de combustível durante o percurso foi de ~ {consumoPorKm} km/l");
+
+            Console.ReadLine();
+        }
+    }
+}
